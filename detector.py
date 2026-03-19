33b22b1b35f2d329eb91194c505078e8b1346a2a
jsediazr <jsediazr@udistrital.edu.co>
2026-03-19 02:07:27 +0000

    Resultados

--- detector.py
+++ detector.py
@@ -10,13 +10,14 @@ resultados = modelo(imagen)
 
 lineas = []
 
-# ─────────────────────────────────────────
-# TU CÓDIGO VA AQUÍ
-# El archivo resultados.txt debe quedar así:
-#
-# Imagen elegida: image1.jpeg
-# Etiquetas detectadas: ['car', 'person', 'bus']
-# ─────────────────────────────────────────
+etiquetas = []
+for r in resultados:
+    for clase_id in r.boxes.cls: 
+        nombre_clase = r.names[int(clase_id)] 
+        etiquetas.append(nombre_clase)
+
+lineas.append(f"Imagen elegida: {imagen}\n")
+lineas.append(f"Etiquetas detectadas: {etiquetas}\n")
 
 # Guardar — no modificar
 with open("resultados.txt", "w", encoding="utf-8") as f:
